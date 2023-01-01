2760a316ddd24485b19ed68ec1d2f8a86b780e45
shuncoder <vinhgatapkich@gmail.com>
2023-01-01 21:05:37 +0700

    tinh ke thua

---
+++ Student.java
@@ -0,0 +1,22 @@
+public class Student extends Person {
+	private double gpa;
+
+	public Student(String name, int age, String address, double gpa) {
+		super(name, age, address);
+		this.gpa = gpa;
+	}
+
+	public double getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+
+	@Override
+	public void display() {
+		super.display();
+		System.out.println("GPA: " + gpa);
+	}
+}
